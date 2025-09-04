257d2d24c85c1fcedd4cebfaf9750457fecd7af6
Kyloo0 <voltaniefitto@gmail.com>
2025-09-04 09:44:06 +0700

    Make function views and make template main.html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main_page(request):
+    context = {
+        'name' : 'Fitto Fadhelli Voltanie Ariyana',
+        'npm' : '2406423401',
+        'class' : 'PBP F'
+    }
+
+    return render(request, 'main.html', context)
